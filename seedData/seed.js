3620097b945fce81dc3c1154aad69b2cd399e28f
David Clarke <107880611+CDGADonc@users.noreply.github.com>
2022-08-30 15:13:58 +0100

    Database Defined

--- seedData/seed.js
+++ seedData/seed.js
@@ -1,23 +1,24 @@
-//This is a snippet for populating the database with seed data.
+// This is a snippet for populating the database with seed data.
 
-// const {sauces, items} = require('./seedData.js');
+const {sauces, items} = require('./seedData.js');
 
-// const {sequelize} = require('./db');
-// const {Item} = require('./models');
+const {database} = require('../server/db');
+const {Item, Sauce} = require('../server/models');
 
-// const seed = async () => {
 
-//     try {
-//         // drop and recreate tables per model definitions
-//         await sequelize.sync({ force: true });
+const seed = async () => {
+
+    try {
+        // drop and recreate tables per model definitions
+        await database.sync({ force: true });
     
-//         // insert data
-//         await Promise.all(items.map(item => Item.create(item)));
+        // insert data
+        await Promise.all(items.map(item => Item.create(item)));
+        await Promise.all(items.map(item => Sauce.create(item)));
+        console.log("db populated!");
+    } catch (error) {
+        console.error(error);
+    }
+}
 
-//         console.log("db populated!");
-//     } catch (error) {
-//         console.error(error);
-//     }
-// }
-
-// seed();
+seed();
