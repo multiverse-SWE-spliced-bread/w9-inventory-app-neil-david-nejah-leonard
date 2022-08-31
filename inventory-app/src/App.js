47ffb9c30a68b3fc1159ae6f1514f5dc3ff460e4
David Clarke <david26th@gmail.com>
2022-08-31 17:10:13 +0100

    Single View prototype

--- inventory-app/src/App.js
+++ inventory-app/src/App.js
@@ -6,14 +6,23 @@ import React, {useState, useEffect} from "react"
 function App() {
 const [selectedItem, setSelectedItem] = useState({})
 const [items , setItems  ] = useState ([])
+const [view, setView] = useState(0)
+const [currentView, setCurrentView] = useState({})  //this is what was using selectedItem
+
+
+const views = [
+  
+  <ItemList items={items} selectedItem={selectedItem} setSelectedItem={setSelectedItem} setView={setView} />
+,
+  <p>{JSON.stringify(selectedItem)}</p>,
+  <p>view3</p>
+ ]
 
 async function fetchItems (){
     const response = await fetch ('http://localhost:3000/item')
     const itemData = await response.json()
     console.log(itemData)
     setItems(itemData)
-
-    
 }
 
 useEffect(()=>{
@@ -21,12 +30,12 @@ useEffect(()=>{
   }, [])
 
 
-  return (
-    <div className="App">
-       <ItemList items={items} selectedItem={selectedItem} setSelectedItem={setSelectedItem} />
-       <ContentDisplay selectedItem={selectedItem} setSelectedItem={setSelectedItem} fetchItems={fetchItems} />
+  return <>
+  <div className="App">
+    {/* views is array of views, view is the piece of state for view */}
+     {views[view]}   
     </div>
-  );
+ </>
 }
 
 export default App;
--- inventory-app/src/ItemList.js
+++ inventory-app/src/ItemList.js
@@ -2,30 +2,34 @@ import React from 'react';
 // import apiURL from './api';
 
 
-export const ItemList =({items,selectedItems,setSelectedItems}) => {
+export const ItemList =({items,setItems, selectedItem,setSelectedItem, setView, setCurrentView}) => {
 
- 
+
+const handleClick = pageObject =>{
+    console.log('lemon')
+    setView(1)
+    setSelectedItem(pageObject)
+    
+
+}
+
 
     return <>
 {/* maps over items, displays in divs to look pretty */}
 {items.map((anObjectMapped, index) => {
     return (
-        <div className="itemCard" key={index}>
-            <img className='displayImage' src={anObjectMapped.image} alt={anObjectMapped.title}/>
+        <div className="itemCard" key={index} onClick={()=> handleClick(anObjectMapped)} >
+            
+            <img className='displayImage' src={anObjectMapped.image} alt={anObjectMapped.title} />
+            
               <ul>
                 <li>{anObjectMapped.title}</li>
                 <li>£{anObjectMapped.price}</li>
-                {/* <li>{anObjectMapped.description}</li> */}
-                {/* <li>{anObjectMapped.category}</li> */}
               </ul>
-                
         </div>
     );
 })}
-
     </>
-
-
 }
 
 
