6dde214b234d4cf98e76c9b1a9385ddbac79c0f3
Neil1977 <Neil_lunn1977@hotmail.com>
2022-09-02 10:19:07 +0100

    updated

--- seedData/seedData.js
+++ seedData/seedData.js
@@ -1,12 +1,8 @@
 module.exports = {
-  sauces: [
-    {"name" : "Sriracha", "image" : "https://64.media.tumblr.com/12cd128183713bf928bda87a24f22487/tumblr_ne6jee85PM1tha1vgo1_250.gif"},
-    {"name" : "Franks", "image": "https://cdn.streamelements.com/uploads/1887a0da-3642-4795-b5f5-774d5b377e52.gif"},
-    {"name" : "Tobasco", "image": "https://64.media.tumblr.com/1564fcf74adc3fe6542c34b6a1f05de1/tumblr_na9lew0bNS1tha1vgo1_r1_250.gif"}
-  ],
+  
   items: [
     {
-       "title":"Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops",
+       "title":"Fjallraven Foldsack No. 1 Backpack, Fits 15 Laptops",
        "price":109.95,
        "description":"Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday",
        "category":"men's clothing",
@@ -20,7 +16,7 @@ module.exports = {
       "image":"https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg"
    },
     {
-       "title":"Mens Casual Premium Slim Fit T-Shirts ",
+       "title":"Mens Casual Premium Slim Fit T Shirts ",
        "price":22.30,
        "description":"Slim-fitting style, contrast raglan long sleeve, three-button henley placket, light weight & soft fabric for breathable and comfortable wearing. And Solid stitched shirts with round neck made for durability and a great fit for casual fashion wear and diehard baseball fans. The Henley style round neckline includes a three-button placket.",
        "category":"men's clothing",
@@ -69,14 +65,14 @@ module.exports = {
        "image":"https://fakestoreapi.com/img/51UDEzMJVpL._AC_UL640_QL65_ML3_.jpg"
     },
     {
-       "title":"WD 2TB Elements Portable External Hard Drive - USB 3.0 ",
+       "title":"WD 2TB Elements Portable External Hard Drive  USB 3.0 ",
        "price":64.00,
        "description":"USB 3.0 and USB 2.0 Compatibility Fast data transfers Improve PC Performance High Capacity; Compatibility Formatted NTFS for Windows 10, Windows 8.1, Windows 7; Reformatting may be required for other operating systems; Compatibility may vary depending on user’s hardware configuration and operating system",
        "category":"electronics",
        "image":"https://fakestoreapi.com/img/61IBBVJvSDL._AC_SY879_.jpg"
     },
     {
-       "title":"SanDisk SSD PLUS 1TB Internal SSD - SATA III 6 Gb/s",
+       "title":"SanDisk SSD PLUS 1TB Internal SSD  SATA III 6 Gb/s",
        "price":109.00,
        "description":"Easy upgrade for faster boot up, shutdown, application load and response (As compared to 5400 RPM SATA 2.5” hard drive; Based on published specifications and internal benchmarking tests using PCMark vantage scores) Boosts burst write performance, making it ideal for typical PC workloads The perfect balance of performance and reliability Read/write speeds of up to 535MB/s/450MB/s (Based on internal testing; Performance may vary depending upon drive capacity, host device, OS and application.)",
        "category":"electronics",
@@ -97,21 +93,21 @@ module.exports = {
        "image":"https://fakestoreapi.com/img/61mtL65D4cL._AC_SX679_.jpg"
     },
     {
-       "title":"Acer SB220Q bi 21.5 inches Full HD (1920 x 1080) IPS Ultra-Thin",
+       "title":"Acer SB220Q bi 21.5 inches Full HD (1920 x 1080) IPS UltraThin",
        "price":599.00,
        "description":"21. 5 inches Full HD (1920 x 1080) widescreen IPS display And Radeon free Sync technology. No compatibility for VESA Mount Refresh Rate: 75Hz - Using HDMI port Zero-frame design | ultra-thin | 4ms response time | IPS panel Aspect ratio - 16: 9. Color Supported - 16. 7 million colors. Brightness - 250 nit Tilt angle -5 degree to 15 degree. Horizontal viewing angle-178 degree. Vertical viewing angle-178 degree 75 hertz",
        "category":"electronics",
        "image":"https://fakestoreapi.com/img/81QpkIctqPL._AC_SX679_.jpg"
     },
     {
-       "title":"Samsung 49-Inch CHG90 144Hz Curved Gaming Monitor (LC49HG90DMNXZA) – Super Ultrawide Screen QLED ",
+       "title":"Samsung 49 Inch CHG90 144Hz Curved Gaming Monitor (LC49HG90DMNXZA)  Super Ultrawide Screen QLED ",
        "price":999.99,
        "description":"49 INCH SUPER ULTRAWIDE 32:9 CURVED GAMING MONITOR with dual 27 inch screen side by side QUANTUM DOT (QLED) TECHNOLOGY, HDR support and factory calibration provides stunningly realistic and accurate color and contrast 144HZ HIGH REFRESH RATE and 1ms ultra fast response time work to eliminate motion blur, ghosting, and reduce input lag",
        "category":"electronics",
        "image":"https://fakestoreapi.com/img/81Zt42ioCgL._AC_SX679_.jpg"
     },
     {
-       "title":"BIYLACLESEN Women's 3-in-1 Snowboard Jacket Winter Coats",
+       "title":"BIYLACLESEN Women's 3 in 1 Snowboard Jacket Winter Coats",
        "price":56.99,
        "description":"Humanized Design: Adjustable and Detachable Hood and Adjustable cuff to prevent the wind and water,for a comfortable fit. 3 in 1 Detachable Design provide more convenience, you can separate the coat and inner as needed, or wear it together. It is suitable for different season and help you adapt to different climates",
        "category":"women's clothing",
